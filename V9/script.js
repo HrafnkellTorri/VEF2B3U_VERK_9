ce97f713563db4d6db385c1cf9dbb8c6b634ab8a
HrafnkellTorri <Kallinnhannhrabbi@hotmail.com>
2017-10-26 12:00:42 +0000

    Add files via upload

--- V9/script.js
+++ V9/script.js
@@ -2,16 +2,14 @@ $(document).ready(function() {
     
     $("#Q1").html("150 million kilometers");
     $("#Q2").html("200 million kilometers");
-    	
+
     	function Change() {
     			$("#Q").fadeOut("Slow", function(){
   		 		$("#Q1").html("284,400 km");
   		 		$("#Q2").html("384,400 km");
   		 		$("p").html("What is the distance to the moon?");
   		 		$("div").css("width","235");
-  		 		$("div").fadeIn("Slow", function(){
-  		 		});
-
+  		 		$("div").fadeIn("Slow");
   		 	});}
 
     	$( "#Q1" ).on("click",function() {
